e316515ca6b8c59ef550ec6a3c502925dcddd3f0
Zlatko Ladan <zl222ag@student.lnu.se>
2015-05-22 11:24:05 +0200

    Lab 1, step 4. Comments + functions's order + something more.

--- step4/1dv434.s4.l1/mediaregister.cpp
+++ step4/1dv434.s4.l1/mediaregister.cpp
@@ -17,49 +17,7 @@
 using std::vector;
 using std::invalid_argument;
 
-void MediaRegister::saveReg(const char *a_dbFile /* "media.dat" */) {
-	if (a_dbFile == NULL) {
-		throw invalid_argument("The database filename cannot be NULL!");
-	}
-
-	MediaDbWriter *writer = NULL;
-
-	try {
-		writer = new MediaDbWriter(a_dbFile);
-	} catch (...) {
-		return;
-	}
-
-	for (vector<BaseMedia *>::iterator i = m_media.begin(); i != m_media.end();
-			++i) {
-		writer->writeNext(*i);
-	}
-
-	delete writer;
-}
-
-void MediaRegister::loadReg(const char *a_dbFile /* "media.dat" */) {
-	if (a_dbFile == NULL) {
-		throw invalid_argument("The database filename cannot be NULL!");
-	}
-
-	MediaDbReader *reader = NULL;
-
-	try {
-		reader = new MediaDbReader(a_dbFile);
-	} catch (...) { // TODO CATCH PROPERLY!!!
-		return;
-	}
-
-	BaseMedia *media = NULL;
-
-	while ((media = reader->readNext()) != NULL) {
-		addMedia(media);
-	}
-
-	delete reader;
-}
-
+// Finds the specified media.
 BaseMedia *MediaRegister::findMedia(const char *a_artistName,
 		const char *a_albumName) {
 	vector<BaseMedia *>::iterator tmp = std::find_if(m_media.begin(),
@@ -80,4 +38,39 @@ BaseMedia *MediaRegister::findMedia(const char *a_artistName,
 	}
 
 	return *tmp;
+}
+
+// Saves register to file.
+void MediaRegister::saveReg(const char *a_dbFile /* "media.dat" */)
+		throw (invalid_argument) {
+	if (a_dbFile == NULL) {
+		throw invalid_argument("The database filename cannot be NULL!");
+	}
+
+	MediaDbWriter *writer = new MediaDbWriter(a_dbFile);
+
+	for (vector<BaseMedia *>::iterator i = m_media.begin(); i != m_media.end();
+			++i) {
+		writer->writeNext(*i);
+	}
+
+	delete writer;
+}
+
+// Loads register from file.
+void MediaRegister::loadReg(const char *a_dbFile /* "media.dat" */)
+		throw (invalid_argument) {
+	if (a_dbFile == NULL) {
+		throw invalid_argument("The database filename cannot be NULL!");
+	}
+
+	MediaDbReader *reader = new MediaDbReader(a_dbFile);
+
+	BaseMedia *media = NULL;
+
+	while ((media = reader->readNext()) != NULL) {
+		addMedia(media);
+	}
+
+	delete reader;
 }
--- step4/1dv434.s4.l1/mediaregister.h
+++ step4/1dv434.s4.l1/mediaregister.h
@@ -16,22 +16,21 @@
 #include "musicalbummedia.h"
 #include <iostream>
 
-using std::cout;
-using std::endl;
-
 class MediaRegister {
 	std::vector<BaseMedia *> m_media;
 
 	// Is used together with showMedia's for_each!
 	static void show(BaseMedia *a_media) {
-		cout << *a_media << endl;
+		std::cout << *a_media << std::endl;
 	}
 
-	// is there a better way? (Not a rhetorical question)
-	static bool sorter(BaseMedia *a_amedia, BaseMedia *a_bmedia) {
-		return *a_amedia < *a_bmedia;
+	// Is there a better way? (Not a rhetorical question)
+	// Compares media
+	static bool sorter(BaseMedia *a_aMedia, BaseMedia *a_bMedia) {
+		return *a_aMedia < *a_bMedia;
 	}
 
+	// Deletes a media from register (to be used with for loop).
 	static void empty(BaseMedia *a_media) {
 		delete a_media;
 	}
@@ -54,24 +53,32 @@ public:
 		std::remove(m_media.begin(), m_media.end(), a_media);
 	}
 
+	// Finds the specified media.
 	BaseMedia *findMedia(const char *artistName, const char *albumName);
 
+	// Shows data for media.
 	void showMedia() {
 		std::for_each(m_media.begin(), m_media.end(), show);
 	}
 
+	// Sorts media.
 	void sortMedia() {
 		// Is there a better way? (Not a rhetorical question)
 		std::sort(m_media.begin(), m_media.end(), sorter);
 	}
 
+	// Removes all data from register.
 	void emptyReg() {
 		std::for_each(m_media.begin(), m_media.end(), empty);
 		m_media.empty();
 	}
 
-	void saveReg(const char *a_dbFile = "media.dat");
-	void loadReg(const char *a_dbFile = "media.dat");
+	// Saves register to file.
+	void saveReg(const char *a_dbFile = "media.dat")
+			throw (std::invalid_argument);
+	// Loads register from file.
+	void loadReg(const char *a_dbFile = "media.dat")
+			throw (std::invalid_argument);
 };
 
 #endif // MEDIAREGISTER_H_
